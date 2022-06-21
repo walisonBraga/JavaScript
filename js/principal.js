8ed6f0c290c3f41453d4bb2b125ae463bf787063
walisonBraga <walison.lemos@gmail.com>
2022-06-21 12:02:03 -0500

    Update principal.js

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,27 @@
-console.log("Olá walison lemos braga")
-
 var titulo = document.querySelector("h2");
-
 titulo.textContent = "Aparecida  Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente")
+
+var tdPeso = paciente.querySelector(".info-peso");
+
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var imc = peso / (altura * altura);
+
+tdImc.textContent = imc;
+
+
+
+
+
+
+//console.log(tdPeso); //tr
+//console.log(paciente); //td que tem o peso
+//console.log(tdAltura);// altura
+//console.log(peso);// obter 100
