5f205e9efbc1b4f2c4717dc3150fad83bba3b503
walisonBraga <walison.lemos@gmail.com>
2022-06-21 11:16:29 -0500

    Update principal.js

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,4 @@
 console.log("Olá walison lemos braga")
 
-var titulo = document.querySelector(".titulo");
+var titulo = document.querySelector("h3");
 titulo.textContent = "Aparecida  Nutricionista";
